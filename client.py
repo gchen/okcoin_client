c95ba82e901da04702cd6238030b82300350e7f3
gchen <chenguancheng@gmail.com>
2014-02-09 11:57:59 +0800

    update readme and add more comments

--- client.py
+++ client.py
@@ -8,10 +8,15 @@ secret_key = ''
 okclient = okcoin.OkCoin(partner_key, secret_key)
 
 def main():
+	#获取市场深度
 	print okclient.get_depth()	
+	#获取账户信息
 	print okclient.get_account()
+	#下单交易
 	order_id = okclient.place_order('btc_cny', 'buy', '47.62', '0.01')	
+	#取消订单
 	okclient.cancel_order(str( 'btc_cny'), order_id["order_id"])
+	#查看交易订单状态
 	print okclient.check_order(str('btc_cny'), order_id['order_id'] )['orders'][0]['status']
 	
 if __name__ == "__main__":
